485804475dcc03862eeb662a3b692eb891f83967
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-08-06 00:28:42 +0200

    init

---
+++ app/about/page.tsx
@@ -0,0 +1,9 @@
+import Link from "next/link";
+export default function About() {
+  return (
+    <>
+      <h1>About us page</h1>
+      <Link href="/">Home </Link>
+    </>
+  );
+}
--- app/page.tsx
+++ app/page.tsx
@@ -1,10 +1,11 @@
-import Image from 'next/image'
-import styles from './page.module.css'
-
+import Image from "next/image";
+import styles from "./page.module.css";
+import Link from "next/link";
 export default function Home() {
   return (
     <main className={styles.main}>
-   <h1>Heloo World !  </h1>
+      <h1>Heloo World ! </h1>
+      <Link href="/about">about us</Link>
     </main>
-  )
+  );
 }
