ce9c90924ab787e8857811ec88cbda6c7ca85687
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-08-08 03:52:16 +0200

    product details

--- app/Products/[slug]/page.tsx
+++ app/Products/[slug]/page.tsx
@@ -1,16 +1,41 @@
-// [slug]/page.tsx
 'use client';
 
-import { useRouter } from 'next/navigation';
 import { useParams } from 'next/navigation';
-
+import data from '@/lib/data';
+import Image from 'next/image';
 const ProductDetails = () => {
-  const router = useRouter();
   const { slug } = useParams();
-
-  console.log(router);
-
-  return <div>Product details page{slug}</div>;
+  const product = data.products.find((item) => item.slug === slug);
+  if (!product) return <div>Product not Found !</div>;
+  return (
+    <>
+      <section className="grid grid-cols-3 justify-items-center">
+        <div>
+          <Image
+            src={product.image}
+            alt={product.name}
+            width={640}
+            height={640}
+            sizes="100vw"
+            style={{
+              width: '100%',
+              height: 'auto',
+            }}
+          ></Image>
+        </div>
+        <div>
+          <p>{product?.brand}</p>
+          <p>{product.price}</p>
+          <p>{product.numReviews}</p>
+          <p>{product.rating}</p>
+          <p>{product.stockStatus}</p>
+        </div>
+        <div>
+          <button className="card"> Add to card</button>
+        </div>
+      </section>
+    </>
+  );
 };
 
 export default ProductDetails;
