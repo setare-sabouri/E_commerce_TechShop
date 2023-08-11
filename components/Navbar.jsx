c2ac2e6d7447c6120c90dfce5424d471c089f674
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-08-11 15:57:22 +0200

    to jsx

--- components/Navbar.jsx
+++ components/Navbar.jsx
@@ -1,32 +1,19 @@
-'use client';
 import Link from 'next/link';
-import { useState, FormEvent } from 'react';
-import { useRouter } from 'next/navigation';
 import { useContext } from 'react';
 import { Store } from '@/utils/Store';
 import ShoppingCartOutlinedIcon from '@mui/icons-material/ShoppingCartOutlined';
 
 const Navbar = () => {
-  const [search, setSeach] = useState('');
-  const router = useRouter();
-  const handleSubmit = (e: FormEvent<HTMLFormElement>) => {
-    e.preventDefault();
-    setSeach('');
-    router.push(`/${search}/`);
-  };
-
-  const { state } = useContext(Store);
-  const { cart } = state;
+  // const { state } = useContext(Store);
+  // const { cart } = state;
   return (
     <nav className="bg-white p-2 flex justify-between items-center border-b">
       <h1 className="text-2xl font-bold">
         <Link href="/">ICON</Link>
       </h1>
-      <form onSubmit={handleSubmit}>
+      <form>
         <input
           type="text"
-          value={search}
-          onChange={(e) => setSeach(e.target.value)}
           placeholder="search"
           className="p-2 w-3/4 text-xl rounded-xl"
           aria-label="Search"
@@ -37,11 +24,11 @@ const Navbar = () => {
       <div className="flex flex-row justify-between gap-4">
         <Link href="/" className="p-2">
           <ShoppingCartOutlinedIcon />
-          {cart && cart.cartItems && cart.cartItems.length > 0 && (
+          {/* {cart && cart.cartItems && cart.cartItems.length > 0 && (
             <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
               {cart.cartItems.length}
             </span>
-          )}
+          )} */}
         </Link>
 
         <Link href="/">
