45e7a04e2866c95f75498358ec6e1a3894139e73
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-08-11 18:35:22 +0200

    myb fixed !

--- app/page.jsx
+++ app/page.jsx
@@ -1,5 +1,9 @@
 import Slider from '../components/Slider';
 
 export default function Home() {
-  return <Slider></Slider>;
+  return (
+    <main className="flex min-h-screen flex-col items-center justify-between p-24">
+      <Slider></Slider>
+    </main>
+  );
 }
