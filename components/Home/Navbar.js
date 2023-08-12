7f0b0cc1f7eb220736b8545cbeba3abf432bbc19
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-08-12 22:09:44 +0200

    cookies

--- components/Home/Navbar.js
+++ components/Home/Navbar.js
@@ -2,9 +2,14 @@ import React, { useContext } from 'react';
 import Link from 'next/link';
 
 import Store from '../../utils/Store';
+import { useState, useEffect } from 'react';
 const Navbar = () => {
     const { state } = useContext(Store);
     const { cart } = state;
+    const [cartItemsCount, setCartItemsCount] = useState(0);
+    useEffect(() => {
+        setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
+    }, [cart.cartItems]);
     return (
         <nav className="flex h-12 items-center px-4 justify-between shadow-md">
             <Link href="/">
@@ -14,9 +19,9 @@ const Navbar = () => {
                 <Link href="/cart">
                     <a className="p-2">
                         Cart
-                        {cart.cartItems.length > 0 && (
+                        {cartItemsCount > 0 && (
                             <span className="ml-1 rounded-full bg-red-600 px-2 py-1 text-xs font-bold text-white">
-                                {cart.cartItems.reduce((a, c) => a + c.quantity, 0)}
+                                {cartItemsCount}
                             </span>
                         )}
                     </a>
