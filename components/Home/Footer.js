c81c1031acf2286416c8c099f37662b5fc4026be
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-08-17 21:49:11 +0200

    responsives footer

--- components/Home/Footer.js
+++ components/Home/Footer.js
@@ -1,15 +1,28 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
 
 const Footer = () => {
     const [activeAccordion, setActiveAccordion] = useState(null);
+    const [isWideScreen, setIsWideScreen] = useState();
 
     const toggleAccordion = (index) => {
         setActiveAccordion(activeAccordion === index ? null : index);
     };
+
+    useEffect(() => {
+        const handleResize = () => {
+            setIsWideScreen(window.innerWidth >= 768);
+        };
+
+        window.addEventListener('resize', handleResize);
+
+        return () => {
+            window.removeEventListener('resize', handleResize);
+        };
+    }, []);
     return (
         <footer className="bg-gray-900 text-white">
 
-            <section className="p-4 grid  grid-cols-1 md:grid-cols-2">
+            <section className="p-4 grid grid-cols-1 gap-1 md:grid-cols-2">
                 <div className='flex flex-col'>
                     <h2 className="text-xl font-bold mb-2">Subscribe to Our Newsletter</h2>
                     <p>Be the first to hear about the latest offers.</p>
@@ -18,7 +31,7 @@ const Footer = () => {
                 <div className="flex gap-2">
                     <input
                         type="email"
-                        className="border border-gray-400 rounded px-2 py-1 flex-grow "
+                        className="border border-gray-400 rounded px-2 py-1 flex-grow-0 md:flex-grow "
                         placeholder="Enter your email"
                     />
                     <button className="bg-blue-500 hover:bg-blue-600 text-white rounded px-4 py-1">
@@ -29,65 +42,150 @@ const Footer = () => {
 
 
             <section className="p-4">
-                <div className="grid grid-cols-1 sm:grid-cols-2 md:grid-cols-5 gap-4">
+                <div className="grid grid-cols-1  md:grid-cols-5 gap-2">
                     <article className="mb-4 md:mb-0">
-                        <h3 className="text-lg font-bold mb-2 cursor-pointer" onClick={() => toggleAccordion(1)} >
-                            Information
-                        </h3>
-                        {activeAccordion === 1 && (
-                            <ul>
-                                <li>Item 1</li>
-                                <li>Item 2</li>
-                                <li>Item 3</li>
-                            </ul>
+                        {!isWideScreen && (
+                            <div>
+                                <h3 className="text-lg font-bold mb-2 cursor-pointer" onClick={() => toggleAccordion(1)} >
+                                    Information
+                                </h3>
+                                {activeAccordion === 1 && (
+                                    <ul>
+                                        <li>Item 1</li>
+                                        <li>Item 2</li>
+                                        <li>Item 3</li>
+                                    </ul>
+                                )}
+                            </div>
+                        )
+                        }
+                        {isWideScreen && (
+                            <div>
+                                <h3 className="text-lg font-bold mb-2 cursor-pointer">
+                                    PC Parts
+                                </h3>
+                                <ul>
+                                    <li>Item 1</li>
+                                    <li>Item 2</li>
+                                    <li>Item 3</li>
+                                </ul>
+                            </div>
                         )}
                     </article>
                     <article className="mb-4 md:mb-0">
-                        <h3 className="text-lg font-bold mb-2 cursor-pointer" onClick={() => toggleAccordion(2)} >
-                            PC Parts
-                        </h3>
-                        {activeAccordion === 2 && (
-                            <ul>
-                                <li>Item 1</li>
-                                <li>Item 2</li>
-                                <li>Item 3</li>
-                            </ul>
+                        {!isWideScreen && (
+                            <div>
+                                <h3 className="text-lg font-bold mb-2 cursor-pointer" onClick={() => toggleAccordion(2)} >
+                                    PC Parts
+                                </h3>
+                                {activeAccordion === 2 && (
+                                    <ul>
+                                        <li>Item 1</li>
+                                        <li>Item 2</li>
+                                        <li>Item 3</li>
+                                    </ul>
+                                )}
+                            </div>
+                        )
+                        }
+                        {isWideScreen && (
+                            <div>
+                                <h3 className="text-lg font-bold mb-2 cursor-pointer">
+                                    Information
+                                </h3>
+                                <ul>
+                                    <li>Item 1</li>
+                                    <li>Item 2</li>
+                                    <li>Item 3</li>
+                                </ul>
+                            </div>
                         )}
                     </article>
                     <article className="mb-4 md:mb-0">
-                        <h3 className="text-lg font-bold mb-2 cursor-pointer" onClick={() => toggleAccordion(3)} >
-                            Desktop Parts
-                        </h3>
-                        {activeAccordion === 3 && (
-                            <ul>
-                                <li>Item 1</li>
-                                <li>Item 2</li>
-                                <li>Item 3</li>
-                            </ul>
+                        {!isWideScreen && (
+                            <div>
+                                <h3 className="text-lg font-bold mb-2 cursor-pointer" onClick={() => toggleAccordion(3)} >
+                                    Desktop Prts
+                                </h3>
+                                {activeAccordion === 3 && (
+                                    <ul>
+                                        <li>Item 1</li>
+                                        <li>Item 2</li>
+                                        <li>Item 3</li>
+                                    </ul>
+                                )}
+                            </div>
+                        )
+                        }
+                        {isWideScreen && (
+                            <div>
+                                <h3 className="text-lg font-bold mb-2 cursor-pointer">
+                                    Desktop Prts
+                                </h3>
+                                <ul>
+                                    <li>Item 1</li>
+                                    <li>Item 2</li>
+                                    <li>Item 3</li>
+                                </ul>
+                            </div>
                         )}
                     </article>
                     <article className="mb-4 md:mb-0">
-                        <h3 className="text-lg font-bold mb-2 cursor-pointer" onClick={() => toggleAccordion(4)} >
-                            Laptops
-                        </h3>
-                        {activeAccordion === 4 && (
-                            <ul>
-                                <li>Item 1</li>
-                                <li>Item 2</li>
-                                <li>Item 3</li>
-                            </ul>
+                        {!isWideScreen && (
+                            <div>
+                                <h3 className="text-lg font-bold mb-2 cursor-pointer" onClick={() => toggleAccordion(4)} >
+                                    Laptops
+                                </h3>
+                                {activeAccordion === 4 && (
+                                    <ul>
+                                        <li>Item 1</li>
+                                        <li>Item 2</li>
+                                        <li>Item 3</li>
+                                    </ul>
+                                )}
+                            </div>
+                        )
+                        }
+                        {isWideScreen && (
+                            <div>
+                                <h3 className="text-lg font-bold mb-2 cursor-pointer">
+                                    Laptops
+                                </h3>
+                                <ul>
+                                    <li>Item 1</li>
+                                    <li>Item 2</li>
+                                    <li>Item 3</li>
+                                </ul>
+                            </div>
                         )}
                     </article>
                     <article className="mb-4 md:mb-0">
-                        <h3 className="text-lg font-bold mb-2 cursor-pointer" onClick={() => toggleAccordion(5)} >
-                            Address
-                        </h3>
-                        {activeAccordion === 5 && (
-                            <ul>
-                                <li>Item 1</li>
-                                <li>Item 2</li>
-                                <li>Item 3</li>
-                            </ul>
+                        {!isWideScreen && (
+                            <div>
+                                <h3 className="text-lg font-bold mb-2 cursor-pointer" onClick={() => toggleAccordion(5)} >
+                                    Address
+                                </h3>
+                                {activeAccordion === 5 && (
+                                    <ul>
+                                        <li>Item 1</li>
+                                        <li>Item 2</li>
+                                        <li>Item 3</li>
+                                    </ul>
+                                )}
+                            </div>
+                        )
+                        }
+                        {isWideScreen && (
+                            <div>
+                                <h3 className="text-lg font-bold mb-2 cursor-pointer">
+                                    Address
+                                </h3>
+                                <ul>
+                                    <li>Item 1</li>
+                                    <li>Item 2</li>
+                                    <li>Item 3</li>
+                                </ul>
+                            </div>
                         )}
                     </article>
                 </div>
