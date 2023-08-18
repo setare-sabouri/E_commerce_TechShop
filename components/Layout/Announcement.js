a61c5b10bb1fce521e81f1607a9136e1c91122bb
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-08-18 02:51:05 +0200

    announcement detailed

--- components/Layout/Announcement.js
+++ components/Layout/Announcement.js
@@ -1,7 +1,19 @@
 const Announcement = () => {
   return (
-    <div className="bg-blue-500 p-4 text-center" role="alert">
-      <p> Super Deal! Free Shipping on Orders Over $50</p>
+    <div className="bg-black text-white text-[10px] md:text-sm flex gap-4 justify-between p-4 text-center " role="alert">
+      <p>Mon-Thu:  9:00 AM - 5:30 PM</p>
+      <p> Visit our showroom in 1234 Street Adress City Address, 1234  Contact Us</p>
+      <div className="flex gap-1 items-center">
+        <p>Call Us: (00) 1234 5678</p>
+        <a href="#">
+          <img src="footer/facebook.png" alt="Facebook" />
+        </a>
+        <a href="#">
+          <img src="footer/instagram.png" alt="Instagram" />
+        </a>
+      </div>
+
+
     </div>
   );
 };
--- pages/Home/productItem.js
+++ pages/Home/productItem.js
@@ -3,22 +3,22 @@ import React from 'react';
 
 export default function ProductItem({ product }) {
     return (
-        <article className="card">
+        <article className="flex flex-col">
             <Link href={`/product/${product.slug}`} >
                 <img
                     src={product.image}
                     alt={product.name}
-                    className="rounded shadow cursor-pointer p-2"
+                    className="cursor-pointer"
                 />
             </Link>
-            <div className="flex flex-col items-center justify-center p-5">
+            <div className="flex flex-col items-start justify-center p-5">
                 <Link href={`/product/${product.slug}`}>
                     <a>
                         <h2 className="text-lg">{product.name}</h2>
                     </a>
                 </Link>
                 <p className="mb-2">{product.brand}</p>
-                <p>${product.price}</p>
+                <p className="font-bold">${product.price}</p>
             </div>
         </article>
     );
