4cd3364022e5645fe4ce354adafb39d1c23b501c
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-08-20 14:51:30 +0200

    swiper

--- pages/Home/Slider.js
+++ pages/Home/Slider.js
@@ -1,36 +1,28 @@
+import { Swiper, SwiperSlide } from 'swiper/react';
+import 'swiper/css';
+
 import { Navigation, Scrollbar, A11y } from 'swiper/modules';
 
-import { Swiper, SwiperSlide } from 'swiper/react';
-
-
-import 'swiper/css';
 import 'swiper/css/navigation';
-import 'swiper/css/pagination';
 import 'swiper/css/scrollbar';
 
 const Slider = () => {
   return (
-
-    <div className=' className="flex min-h-screen flex-col items-center justify-between p-24"'>
-      <Swiper
-        className="md:w-70 xs:w-60 lg:w-70 h-screen  flex items-center justify-center"
-        modules={[Navigation, Scrollbar, A11y]}
-        spaceBetween={50}
-        slidesPerView={1}
-        navigation
-        pagination={{ clickable: true }}
-        scrollbar={{ draggable: true }}
-        onSwiper={(swiper) => console.log(swiper)}
-        onSlideChange={() => console.log('slide change')}
-      >
-        <SwiperSlide>Slide 1</SwiperSlide>
-        <SwiperSlide>Slide 2</SwiperSlide>
-        <SwiperSlide>Slide 3</SwiperSlide>
-        <SwiperSlide>Slide 4</SwiperSlide>
-      </Swiper>
-    </div>
-
-
+    <Swiper
+      className="md:w-70 xs:w-60 lg:w-70 h-auto  flex items-center justify-center"
+      modules={[Navigation, Scrollbar, A11y]}
+      spaceBetween={50}
+      slidesPerView={1}
+      navigation
+      scrollbar={{ draggable: true }}
+      onSwiper={(swiper) => console.log(swiper)}
+      onSlideChange={() => console.log('slide change')}
+    >
+      <SwiperSlide><img src='./images/slider.png' /></SwiperSlide>
+      <SwiperSlide>Slide 2</SwiperSlide>
+      <SwiperSlide>Slide 3</SwiperSlide>
+      <SwiperSlide>Slide 4</SwiperSlide>
+    </Swiper>
   );
 };
 
