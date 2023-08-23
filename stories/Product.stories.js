19b0cce207ce2d99a59f8a9271e5e5b533508724
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-08-23 18:25:52 +0200

    docs point

--- stories/Product.stories.js
+++
@@ -1,9 +0,0 @@
-import Footer from "../Layout/Footer"
-
-
-export default {
-    title: 'product details',
-    component: Footer,
-    tags: ['autodocs']
-}
-export const pro = () => <Footer label="ss" />
