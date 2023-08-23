4559ba3577f6f4419d05c9304ca9c0b4289be99b
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-08-23 15:20:52 +0200

    user menue

--- Layout/Navbar.js
+++ Layout/Navbar.js
@@ -2,16 +2,20 @@ import React, { useContext, useState, useEffect } from 'react';
 import Link from 'next/link';
 import Store from '../utils/Store';
 import Image from 'next/image';
-import { useSession } from 'next-auth/react';
+import { signOut, useSession } from 'next-auth/react';
 import { ToastContainer } from 'react-toastify';
 import 'react-toastify/dist/ReactToastify.css';
+import { Menu } from '@headlessui/react';
+import Cookies from 'js-cookie';
+import DropdownLink from './DropdownLink';
+
 const Navbar = () => {
     <ToastContainer position="bottom-center" limit={1} />
-
-    const { state } = useContext(Store);
+    const { state, dispatch } = useContext(Store);
     const { cart } = state;
     const [cartItemsCount, setCartItemsCount] = useState(0);
     const { status, data: session } = useSession();
+    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
 
     useEffect(() => {
         setCartItemsCount(cart.cartItems.reduce((a, c) => a + c.quantity, 0));
@@ -21,7 +25,11 @@ const Navbar = () => {
         setMobileMenuOpen(!isMobileMenuOpen);
     };
 
-    const [isMobileMenuOpen, setMobileMenuOpen] = useState(false);
+    const logoutClickHandler = () => {
+        Cookies.remove('cart');
+        dispatch({ type: 'CART_RESET' });
+        signOut({ callbackUrl: '/Login/login' });
+    };
 
     return (
         <nav className="flex flex-col md:flex-row items-center px-4 py-2 justify-between shadow-md">
@@ -50,7 +58,7 @@ const Navbar = () => {
                     </button>
                 </li>
             </ul>
-            <div className="md:flex items-center">
+            <div className="flex gap-2  items-center">
                 <Link href="/Card/cart">
                     <a className="p-2">
                         Cart
@@ -62,7 +70,39 @@ const Navbar = () => {
                     </a>
                 </Link>
                 {status === 'loading' ? ('Loading') :
-                    session?.user ? (session.user.name) : (
+                    session?.user ? (
+                        <div className="relative">
+                            <Menu>
+                                <Menu.Button className="text-blue-600">
+                                    {session.user.name}
+                                </Menu.Button>
+                                <Menu.Items className="fixed z-10 right-0 w-56 origin-top-right bg-white shadow-lg mt-2">
+                                    <Menu.Item>
+                                        <DropdownLink className="dropdown-link" href="/#">
+                                            Profile
+                                        </DropdownLink>
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        <DropdownLink
+                                            className="dropdown-link"
+                                            href="/Card/cart"
+                                        >
+                                            Order History
+                                        </DropdownLink>
+                                    </Menu.Item>
+                                    <Menu.Item>
+                                        <a
+                                            className="dropdown-link"
+                                            onClick={logoutClickHandler}
+                                            href="#"
+                                        >
+                                            Logout
+                                        </a>
+                                    </Menu.Item>
+                                </Menu.Items>
+                            </Menu>
+                        </div>
+                    ) : (
                         <Link href="/Login/login">
                             <a className="p-2">Login</a>
                         </Link>
@@ -75,23 +115,25 @@ const Navbar = () => {
                 Menu
             </button>
             {/* Mobile navigation links */}
-            {isMobileMenuOpen && (
-                <ul className="md:hidden flex flex-col gap-2">
-                    <li className="cursor-pointer">Laptops</li>
-                    <li className="cursor-pointer">Desktop PCs</li>
-                    <li className="cursor-pointer">Networking Devices</li>
-                    <li className="cursor-pointer">Printers & Scanners</li>
-                    <li className="cursor-pointer">PC Parts</li>
-                    <li className="cursor-pointer">All Other Products</li>
-                    <li className="cursor-pointer">Repairs</li>
-                    <li>
-                        <button className="shadow-black border px-6 py-2 rounded-3xl border-blue-800 text-blue-800">
-                            Our Deals
-                        </button>
-                    </li>
-                </ul>
-            )}
-        </nav>
+            {
+                isMobileMenuOpen && (
+                    <ul className="md:hidden flex flex-col gap-2">
+                        <li className="cursor-pointer">Laptops</li>
+                        <li className="cursor-pointer">Desktop PCs</li>
+                        <li className="cursor-pointer">Networking Devices</li>
+                        <li className="cursor-pointer">Printers & Scanners</li>
+                        <li className="cursor-pointer">PC Parts</li>
+                        <li className="cursor-pointer">All Other Products</li>
+                        <li className="cursor-pointer">Repairs</li>
+                        <li>
+                            <button className="shadow-black border px-6 py-2 rounded-3xl border-blue-800 text-blue-800">
+                                Our Deals
+                            </button>
+                        </li>
+                    </ul>
+                )
+            }
+        </nav >
     );
 };
 
---
+++ stories/Product.stories.js
@@ -0,0 +1,9 @@
+import Footer from "../Layout/Footer"
+
+
+export default {
+    title: 'product details',
+    component: Footer,
+    tags: ['autodocs']
+}
+export const pro = () => <Footer label="ss" />
