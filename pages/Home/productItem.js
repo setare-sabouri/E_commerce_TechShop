8e011ce9096fc31435d68735c1264a87446c2e7d
Setareh <setarehsaboory@gmail.com>
2023-09-13 15:22:57 +0200

    a smal bug !

--- pages/Home/productItem.js
+++ pages/Home/productItem.js
@@ -12,7 +12,7 @@ export default function ProductItem({ product }) {
                 />
             </Link>
             <div className="flex flex-col items-start justify-center p-5">
-                <Link href={`/product/${product.slug}`}>
+                <Link href={`/Product/${product.slug}`}>
                     <a>
                         <h2 className="text-lg">{product.name}</h2>
                     </a>
