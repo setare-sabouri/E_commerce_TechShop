c2feac7104431b44c8e2222a29ed7d81fc001e70
Setareh <setarehsaboory@gmail.com>
2023-10-18 14:52:22 +0200

    styling

--- pages/Components/Laptops/Laptops.tsx
+++ pages/Components/Laptops/Laptops.tsx
@@ -1,23 +1,23 @@
-import React from 'react'
+import React from "react";
 import Layout from "../../../Layout/Layout";
-import styles from './Laptops.module.scss'
+import styles from "./Laptops.module.scss";
 import { Canvas } from "@react-three/fiber";
 const Laptops = () => {
+  return (
+    <Layout title="laptops Page">
+      <div className={styles.laptops}>
+        <p className={`${styles.laptops__title} heading1`}>Laptops page</p>
+      </div>
+      <div className={`${styles.laptops__canvas} canvas-wrapper`}>
+        <Canvas>
+          <mesh>
+            <torusKnotGeometry />
+            <meshNormalMaterial />
+          </mesh>
+        </Canvas>
+      </div>
+    </Layout>
+  );
+};
 
-    return (
-        <Layout title="laptops Page">
-            <div className={styles.laptops}>
-            <p className={`${styles.laptops__title} heading1`} >Laptops page</p>
-            </div>
-
-            <Canvas>
-                <mesh>
-                    <torusKnotGeometry />
-                    <meshNormalMaterial />
-                </mesh>
-            </Canvas>
-        </Layout>
-    );
-}
-
-export default Laptops
+export default Laptops;
