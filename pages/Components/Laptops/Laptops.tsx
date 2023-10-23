095e127f13eacce0328d4c7e93187ff17f38d16a
Setareh <setarehsaboory@gmail.com>
2023-10-23 15:44:09 +0200

    excersice on r3f

--- pages/Components/Laptops/Laptops.tsx
+++ pages/Components/Laptops/Laptops.tsx
@@ -2,18 +2,29 @@ import React from "react";
 import Layout from "../../../Layout/Layout";
 import styles from "./Laptops.module.scss";
 import { Canvas } from "@react-three/fiber";
+import { OrbitControls } from "@react-three/drei";
 const Laptops = () => {
   return (
     <Layout title="laptops Page">
       <div className={styles.laptops}>
         <p className={`${styles.laptops__title} heading1`}>Laptops page</p>
       </div>
-      <div className={`${styles.laptops__canvas} canvas-wrapper`}>
-        <Canvas>
-          <mesh>
-            <torusKnotGeometry />
-            <meshNormalMaterial />
+
+      <div className="canvas-wrapper">
+        <Canvas className={`${styles.laptops__canvas} `}>
+          <mesh position={[-1, 1, 0]}>
+            <sphereGeometry />
+            <meshBasicMaterial color="orange" />
           </mesh>
+          <mesh position={[1, 0.5, 0]}>
+            <boxGeometry />
+            <meshBasicMaterial color="lightblue" />
+          </mesh>
+          <mesh rotation-x={-Math.PI * 0.5} scale={5}>
+            <planeGeometry />
+            <meshBasicMaterial color="green" />
+          </mesh>
+          <OrbitControls makeDefault />
         </Canvas>
       </div>
     </Layout>
