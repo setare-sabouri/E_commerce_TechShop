b2e1d561e90f459c8310ad722ecc41d454fec9a3
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2023-11-26 17:23:19 +0100

    border

--- src/components/Product/productItem.tsx
+++ src/components/Product/productItem.tsx
@@ -1,6 +1,6 @@
 import Link from 'next/link';
 import React from 'react';
-import styles from './product.module.scss'; // Import the SCSS module
+import styles from './product.module.scss';
 
 interface ProductItemProps {
     product: {
