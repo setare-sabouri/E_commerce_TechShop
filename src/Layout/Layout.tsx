2930836e4421ea9b2c54146c1c4d35c162b014ba
setare-sabouri <100029656+setare-sabouri@users.noreply.github.com>
2024-11-03 20:05:38 +0100

    rafactor layout

--- src/Layout/Layout.tsx
+++ src/Layout/Layout.tsx
@@ -1,19 +1,20 @@
 import Head from "next/head";
 import Navbar from "@layout/Navbar";
 import Footer from "@layout/Footer";
-import Announcement from "@layout/Announcement";
+import Announcement from "@Layout/Announcement";
+import { ReactNode } from "react";
 
-export default function Layout({
-  title,
-  children,
-}: {
-  title: string;
-  children: any;
-}) {
+interface LayoutProps {
+  title?: string; //optional title
+  children: ReactNode;
+}
+
+const Layout: React.FC<LayoutProps> = ({ title, children }) => {
+  const PageTitle = title ? `${title} - TechShop` : "TechShop"
   return (
     <>
       <Head>
-        <title>{title ? title + " - TechShop" : "TechShop"}</title>
+        <title>{PageTitle}</title>
         <meta name="description" content="Ecommerce Website" />
         <meta name="viewport" content="width=device-width, initial-scale=1.0" />
         <link rel="icon" href="Logo.png" />
@@ -29,3 +30,6 @@ export default function Layout({
     </>
   );
 }
+
+
+export default Layout
